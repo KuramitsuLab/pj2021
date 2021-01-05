3aa180261a47723a1743e079d86e7ae7930449ed
kkuramitsu <kkuramitsu@gmail.com>
2021-01-05 14:38:26 +0900

    tefu417 corpus

---
+++ tefu417/train.py
@@ -0,0 +1,412 @@
+A = B * math . sin ( C )
+if A == 'U' or A == 'L' :
+print ( A , B + C , B * 200 + C * 300 )
+print ( A [ B ] , sep = '' , end = '' )
+A [ 0 ] = A [ 0 ] . lower ( )
+print ( '-' )
+print ( '*' , end = '' )
+for A in range ( B [ C ] ) :
+A [ 0 ] = ( B [ 0 ] - C [ 0 ] ) / 3 * 2 + C [ 0 ]
+import math
+A = B [ 1 : len ( B ) ] . count ( 'A' )
+A = int ( B [ 1 ] + B [ 2 ] , 16 )
+A = list ( input ( ) )
+A . append ( 'shikaku' )
+print ( input ( ) . upper ( ) )
+A [ B + C ] += 1
+A *= 3
+A = B % 1000
+for A in range ( B , C ) :
+print ( 'Open' )
+A . append ( [ int ( B ) for B in input ( ) . split ( ) ] )
+A = math . sqrt ( B ** 2 + ( C - D ) ** 2 + E ** 2 )
+A . append ( int ( input ( ) ) )
+print ( input ( ) . swapcase ( ) )
+A = { 1 : 6000 , 2 : 4000 , 3 : 3000 , 4 : 2000 }
+print ( {S:.6f} )
+A [ B . index ( C [ D ] ) ] += 1
+A [ B ] = A [ B ] . lower ( )
+print ( A , 'x' , B , '=' , A * B , sep = '' )
+A = list ( B )
+A -= 5
+if A [ B ] not in C :
+A = f ( B )
+A [ 0 ] = 0
+A . append ( abs ( B [ C ] - D [ C ] ) )
+A = B [ 0 ]
+break
+A = sum ( B ) / C
+for A in range ( B + 1 , ( C // 2 ) + 3 ) :
+print ( A [ B [ C ] ] , end = '' )
+A = [ 0 for B in range ( 2001 ) ]
+if A == B [ C ] + B [ D ] :
+print ( A [ B ] , ' ' , sep = '' , end = '' )
+A = B - ( C * 3600 + D * 60 )
+A = [ 0 , 0 ]
+A += B [ C - 1 ]
+if A != 1 :
+A = B . findall ( '[0-9]+' , C )
+for A in range ( 2 , int ( math . sqrt ( B ) ) + 1 ) :
+A = 100000
+A , B , C , D , E = map ( int , input ( ) . split ( ) )
+A += abs ( B [ C ] - D [ C ] ) ** 3
+A = [ 0 for B in range ( 26 ) ]
+while A % 2 == 0 :
+A = B // C
+print ( Euc ( A , B , C ) [ 0 ] , Euc ( A , B , C ) [ 1 ] )
+A [ B ] = C . split ( '[,.]' , A [ B ] )
+print ( 0 , 0 )
+A , B = 2 , 1
+return - 1
+if A + B <= C and D + B <= E :
+print ( 0 , A )
+A += B [ C ] [ D ]
+A = [ 0 ] * 45
+A = 1000000
+return 2
+print ( A [ 0 ] , A [ 1 ] )
+print ( A , ':' , sep = '' , end = ' ' )
+print ( abs ( A - B ) )
+for A in range ( 1001 ) :
+A = bo ( B , C , D , E )
+A = B
+A = list ( set ( B ) - set ( C ) )
+A = max ( B ) - min ( B )
+A , B = C , D
+A += abs ( B [ C ] - D [ C ] ) ** 2
+for A in range ( B + 1 , C ) :
+A *= - 1
+A += 40
+A [ 1 ] = ( B [ 1 ] - C [ 1 ] ) / 3 + C [ 1 ]
+if A == '+' :
+for A in range ( 5 ) :
+for A in range ( 10 ) :
+if A ** 2 == B :
+print ( 0 )
+print ( pow ( A , B , C ) )
+A = math . sqrt ( B ** 2 - 4 * C * D )
+print ( 'a > b' )
+print ( )
+if A == - 0 :
+print ( '.' , sep = '' , end = '' )
+if A == - 1 and B == - 1 and C == - 1 :
+print ( A , B , C )
+A = sum ( B )
+A = [ 1 for B in range ( 1000000 ) ]
+for A in range ( 1 , B // 2 + 1 ) :
+A = math . radians ( B )
+A = len ( B )
+A = A - B + 1000
+A = math . sqrt ( B / C )
+A = int ( B [ 5 ] + B [ 6 ] , 16 )
+A = - 2
+A = f ( B ) // 10
+if A == - 1 :
+if A >= 50 :
+A . append ( 'A' )
+while A >= 100 :
+if A * B < C :
+print ( A // B )
+A += B [ C ] * B [ D - C ]
+if A == 0 and B == 'U' :
+Koch ( A - 1 , B , C )
+if A in B :
+A = 'peach'
+A . append ( B [ C ] )
+A [ B ] = fib ( B - 1 ) + fib ( B - 2 )
+if A < 40 :
+A = 600 // B
+return 0
+if A % B == 0 :
+return math . cos ( A )
+print ( A [ B ] , int ( A [ B ] + C ) )
+print ( min ( A ) )
+A = Era ( B , C )
+A , B , C = map ( str , input ( ) . split ( ) )
+if A [ B ] == 0 :
+if A >= 10 and B >= 10 :
+if A [ B ] == ')' :
+print ( A )
+A = B [ C - 1 ]
+return 1
+if A != 0 :
+for A in range ( B - 1 , 0 , - 1 ) :
+while A >= 5 :
+A = [ ]
+A = [ 'K' , 'U' , 'P' , 'C' ]
+if f ( X ( A , B , C ) [ 0 ] ) < f ( X ( A , B , C ) [ 1 ] ) :
+A = [ B for B in range ( 1 , 31 ) ]
+if len ( A ) != 0 :
+A = [ 100.0 , 0.0 ]
+print ( int ( A ) )
+A = ( B - C ) ** 2
+A = len ( B [ C ] )
+if A >= 500 :
+A [ 0 ] = A [ 0 ] . upper ( )
+A += B [ C ] [ D ] * E [ D ] [ F ]
+A = B / 2
+if X ( A , B , C ) == - 1 :
+if A == 'S' :
+A = Near ( B , C ) + 1
+if A == 0 and B == 0 :
+A = [ 0 for B in range ( 4 ) ]
+if ( A == 5 ) :
+print ( 'no' )
+A . remove ( int ( B ) )
+A [ B ] = A [ B ] . upper ( )
+A = A [ B : len ( A ) + 1 ]
+A -= 1
+A *= B
+if A < B and B < C :
+for A in range ( B - 2000 , 2001 ) :
+if A == 'END OF INPUT' :
+print ( A - B )
+A = set ( A )
+if A > B and A <= C :
+A %= B
+if A [ B : B + 5 ] == C :
+A [ B ] = ( C , D , E , F )
+for A in range ( 3 , int ( B ** ( 1 / 2 ) ) + 1 , 2 ) :
+A = [ len ( input ( ) ) for B in range ( C ) ]
+if A < B :
+A . append ( 'C' )
+return math . radians ( A )
+A , B = B , A
+A = B * 2 * math . pi
+A -= ( ( A ** 3 - B ) / ( 3 * A ** 2 ) )
+A [ B . index ( C [ D ] . lower ( ) ) ] += 1
+return ( ( - A + B ) / ( 2 * C ) , ( - A - B ) / ( 2 * C ) )
+print ( 'No' )
+for A in range ( B - 1 , 1 , - 1 ) :
+A = min ( B )
+for A in range ( 9 ) :
+return abs ( A - B // 2 )
+print ( A // 2 + 1 )
+print ( 'B' )
+if 0 < A and B < 0 :
+if A > 0 :
+A . pop ( - 1 )
+A = A * B [ C ] // math . gcd ( A , B [ C ] )
+P ( - A , 3 )
+if A + B + C == D :
+A [ 1 ] = 0
+for A in range ( 4 ) :
+return True
+A += ' '
+print ( A [ B ] , ':' , C [ B ] )
+if ( A == B [ C ] ) :
+A = B [ C ] - D [ E - F ] + D [ E ]
+print ( 'C' )
+print ( 'F' )
+A = { "0" : 0 , "1" : 1 , "2" : 2 , "3" : 3 , "4" : 5 , "5" : 7 , "6" : 8 , "7" : 9 }
+A = B * 2
+if ( A [ B ] + C ) in D :
+if A == B - 1 :
+A = 1000000007
+for A in range ( 26 ) :
+if ( A - B ) / C < 7 :
+A , B , C , D = map ( int , input ( ) . split ( ) )
+A += B
+A . insert ( B , '_' )
+A = ( B * C - D * E ) / ( F * C - D * G )
+A = X ( B , C , D ) [ 1 ]
+A *= 1.05
+A = B // 365 + 1
+A -= 10
+for A in range ( len ( B ) ) :
+if A == 1 :
+A = len ( B ) - C - 1
+while A != 0 :
+A = 1000 - B
+( A [ 1 ] - B [ 1 ] ) * sin ( rad ( 60 ) ) + B [ 0 ]
+if A [ B ] in C or A [ B ] . lower ( ) in C :
+print ( len ( A ) )
+A += int ( input ( ) )
+print ( 'Close' )
+A = [ chr ( B ) for B in range ( 65 , 65 + 26 ) ]
+if len ( A [ B ] ) > C :
+A = abs ( B )
+if ( A > 4000 ) :
+A [ B ] . append ( sum ( A [ B ] ) )
+if A > B :
+while abs ( A ** 3 - B ) >= 0.00001 * B :
+print ( A * B )
+A += ( B - C ) // D
+A += 1
+A = [ 0 for B in range ( 100 ) ]
+print ( 'D' )
+A = list ( map ( B , C ) )
+print ( A , B )
+A = Near2 ( B , C // 2 ) + 1
+A = B = C = 0
+A . append ( B )
+print ( 'Yes' )
+A = B + 1
+A = [ B for B , C in enumerate ( D ) if C == E ]
+A = ( B * ( 100 + C ) // 100 ) + ( D * ( 100 + C ) // 100 )
+A = [ int ( B ) for B in input ( ) . split ( ) ]
+if A == '.' :
+print ( 'C' , A [ B ] )
+A = str ( B )
+A += 2
+A = [ B for B in A if B != '_' ]
+pass
+A . append ( ( ( len ( A ) + 1 ) ** 2 ) * B ** 3 )
+print ( min ( A ) , max ( A ) , sum ( A ) )
+A , B = 1 , 2
+A = B [ C ]
+if A [ B ] != ' ' :
+print ( 'H' , A [ B ] )
+A = input ( ) . split ( )
+print ( - 1 )
+if A % 2 == 0 :
+for A in range ( B - 1 ) :
+for A in range ( 1 , 10 ) :
+A += B [ C ] [ D ] * E [ D ]
+A = 1
+while A >= 50 :
+A += int ( B [ C ] )
+A = 2 * A
+print ( A + B )
+for A in range ( 3 ) :
+print ( 'YES' )
+A = list ( range ( 2 , B ) )
+print ( math . gcd ( A , B ) )
+if A == '?' :
+for A in range ( B , C + 1 ) :
+A = 'a'
+A = B . pop ( B . index ( max ( B ) ) )
+if A == 'X' :
+A = ( B - C * 3600 ) // 60
+if 2 < len ( A [ B ] [ 0 ] ) and len ( A [ B ] [ 0 ] ) < 7 :
+if A [ - 1 ] == 'maru' :
+if ( f ( A ) % 10 == 0 ) :
+A . append ( 'maru' )
+for A in range ( B - 1 , - 1 , - 1 ) :
+A [ 1 ] = ( B [ 1 ] - C [ 1 ] ) / 3 * 2 + C [ 1 ]
+print ( A [ B ] * C )
+print ( A [ B ] , sep = '' , end = ' ' )
+if A [ B ] == '(' :
+A = B * 2 + C * 2
+A = B / C
+if A == len ( B ) - 1 :
+for A in range ( 28 ) :
+for A in range ( len ( B ) - C + 1 ) :
+A . append ( B [ C ] [ 0 ] )
+return A
+if A == '0' :
+A [ 0 ] = ( B [ 0 ] - C [ 0 ] ) / 3 + C [ 0 ]
+A = 3
+A = int ( input ( ) )
+A = input ( )
+A += ( B [ C ] - D ) ** 2
+A = math . sqrt ( B )
+A = list ( input ( ) . split ( ) )
+A = B [ 0 : C ]
+print ( '#' , sep = '' , end = '' )
+if A == B :
+while A % B == 0 :
+A = B * B * math . pi
+for A in range ( B + 1 , C - 2 , - 1 ) :
+print ( A , 0 )
+A = sorted ( B )
+A = math . sqrt ( ( B - C ) ** 2 + ( D - C ) ** 2 + ( E - C ) ** 2 )
+return False
+A [ B - 1 ] += 1
+if A [ B [ C ] ] == 1 :
+A = B . lower ( )
+A , B = map ( int , input ( ) . split ( ) )
+A = [ chr ( B ) for B in range ( 68 , 68 + 23 ) ]
+A = 0
+while True :
+if A == 0 or A == 1 :
+A = B ** 2
+return 4
+if A != B :
+A = f ( 0 )
+A = list ( reversed ( B ) )
+A = int ( B [ 3 ] + B [ 4 ] , 16 )
+A . append ( 0 )
+A = boa ( B , C , D , E , F )
+if A [ 0 ] + A [ 1 ] > B :
+print ( A , ':' , B , ':' , C , sep = '' )
+print ( A // 5 )
+A = 'abcdefghijklmnopqrstuvwxyz'
+if A == - 1 or B == - 1 or A + B < 30 :
+for A in range ( B ) :
+print ( A + 1 )
+A = [ B for B in range ( C - D , C + D + 1 ) ]
+A = 'apple'
+if A . count ( A [ B ] ) > C :
+if A [ B ] in C :
+for A in range ( 2 , 1000000 // B ) :
+print ( A [ B ] [ C ] )
+if A [ B ] != 0 :
+if A == '-' :
+A += 7
+if len ( A ) > 0 :
+A = [ B for B in C if B <= D ]
+if A [ B ] == C :
+A = math . sqrt ( B ** 2 + ( C - D ) ** 2 + ( E - D ) ** 2 )
+if A [ B ] == '0' :
+print ( A [ B ] [ C ] , end = ' ' )
+print ( 3 , 0 )
+for A in range ( 1000 ) :
+print ( 'NONE' )
+print ( sum ( A ) )
+A = [ '1 1 0' , '0 0 1' ]
+if prime ( int ( input ( ) ) ) :
+A [ int ( input ( ) ) ] += 1
+if A [ B ] == ']' :
+A = B . index ( B [ C ] )
+return math . sin ( A )
+A = list ( map ( int , input ( ) . split ( ) ) )
+A = math . sqrt ( ( B - C ) ** 2 + D ** 2 + ( E - C ) ** 2 )
+if A [ B ] == '[' :
+if A <= B :
+A += abs ( B [ C ] - D [ C ] )
+A += B [ C ]
+A = 3 * A
+A = [ ( 0 , 0 , 0 , 0 ) for B in range ( 10000 ) ]
+A = - 1
+print ( max ( A ) )
+A , B , C = map ( int , input ( ) . split ( ) )
+print ( ( A + B ) // 2 )
+A = [ B , C , D ]
+if A [ - 1 ] == 'shikaku' :
+print ( '#' , end = '' )
+A -= 500
+( A [ 1 ] - B [ 1 ] ) * cos ( rad ( 60 ) ) + B [ 1 ]
+if len ( A ) == 0 :
+del A [ B - 2 ]
+A = B // 365
+if ( A + B ) % 2 == 0 :
+if f ( - A / ( 3 * B ) ) == 0 :
+A = B // ( 7 * C + D )
+A -= 50
+print ( 0 , 3 )
+P ( - A , 1 )
+import re
+A = B - C * D
+if A == 0 :
+if A - B >= 0 and C - B >= 0 :
+if A == 'END_OF_TEXT' :
+for A in range ( 2 ) :
+A = [ 5 , 7 , 5 , 7 , 7 ]
+A = float ( input ( ) )
+A = math . sqrt ( B + C - 2 * D * math . cos ( E ) )
+A = X ( B , C , D ) [ 0 ]
+A = ao ( B , C , D , E )
+A = int ( A )
+print ( A [ B . index ( C ) ] )
+A = list ( format ( B , 'o' ) )
+A = int ( 'ff' , 16 )
+for A in range ( B + 1 ) :
+while A >= 10 :
+A = aob ( B , C , D )
+A = F ( B , C , D , E )
+A = f ( B ) // 10 + 1
+A += ( B - A ) // C * C
+print ( A [ B ] + 1 )
+print ( A [ B ] )
+A = math . sqrt ( ( B - C ) ** 2 + ( D - C ) ** 2 + E ** 2 )
---
+++ tefu417/valid.py
@@ -0,0 +1,51 @@
+A += 7 * B + C
+print ( 'A' )
+return lambda x : A * B ** 3 + C * B ** 2 + D * B + E
+if A [ B ] == C [ B ] :
+if ( A + B + C + D == E ) :
+A , B , C , D = map ( float , input ( ) . split ( ) )
+A [ B * C ] = 0
+if A < 0 :
+A . append ( 'B' )
+if A [ B ] == ')' or A [ B ] == ']' :
+A . append ( B [ C . index ( D [ E ] ) ] )
+return A . index ( B )
+A = [ B for B in range ( C , 1000000 , D ) ]
+A [ B : B + 5 ] = C
+A = B + C + D
+A , B = map ( str , input ( ) . split ( ) )
+for A in range ( 1 , B - 1 ) :
+A = [ B for B in C if D < B <= 2 * D ]
+A . append ( B [ C * D : C * D + D ] )
+print ( A [ B ] , int ( A [ B ] - C ) )
+A , B = 1 , 1
+A = B // 3600
+print ( 'NO' )
+A //= 2
+return ( A , B )
+A = B [ 0 ] ** 2 + B [ 1 ] ** 2
+print ( 'yes' )
+A = B * C
+if A . count ( '(' ) != A . count ( ')' ) or A . count ( '[' ) != A . count ( ']' ) :
+if A [ B : B + 3 ] == 'JOI' :
+A = 2
+del A [ 0 ]
+try :
+if A [ 0 ] == 'A' :
+if A == 0 or A == B - 1 or C == 0 or C == D - 1 :
+A -= 100
+A = [ B , C , D , E ]
+print ( A * 32 )
+A = 123456 * 2
+A = max ( B )
+print ( A [ B ] , end = '' )
+for A in range ( B + 1 , C + 1 ) :
+A = set ( B )
+print ( ( A - 30 ) // 2 )
+print ( A // ( B - 2 ) )
+return abs ( A - B )
+for A in range ( len ( B ) - 2 ) :
+if A [ B : B + C ] == D :
+for A in range ( 2 , 1000000 ) :
+A = sorted ( B , reverse = True )
+return A [ B ]
