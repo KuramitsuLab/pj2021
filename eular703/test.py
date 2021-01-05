fc1535f3ab224702e3ab85d01db1602d28c87fda
Kimio <kkuramitsu@gmail.com>
2021-01-05 22:01:33 +0900

    adding eular703

---
+++ eular703/test.py
@@ -0,0 +1,23 @@
+return A [ 0 ]
+if not A [ B ] :
+A = str ( 2 ** 1000 )
+for A in range ( 2 , B + 1 ) :
+for A in range ( 2 , B ) :
+A -= 1
+if A <= 16 :
+return A <= B
+return sum ( A )
+for A in range ( 999 , 99 , - 1 ) :
+while A >= B or C [ A ] == - 1 :
+A = str ( B )
+while A [ - 1 ] < 4000000 :
+A = [ True ] * ( B + 1 )
+return [ 0 ]
+return A % 400 == 0 or ( ( not A % 100 == 0 ) and A % 4 == 0 )
+return A [ B - 1 ]
+if A <= 2 :
+B = (int) ( len ( A ) / 2 ) + ( len ( A ) % 2 )
+A [ B ] += A [ B + 1 ]
+if 1582 <= A :
+for A in range ( B , B + ( C * D ) , C ) :
+if not A % 2 :
---
+++ eular703/train.py
@@ -0,0 +1,180 @@
+A = max ( A , B [ C * 20 + D ] * B [ ( C + 1 ) * 20 + D ] * B [ ( C + 2 ) * 20 + D ] * B [ ( C + 3 ) * 20 + D ] )
+A = 101 * 50
+if len ( A ) < B :
+if A == 1 :
+A = - 1 * int ( B / 100 ) + 5
+A = [ True ] * 2000000
+return ( A , B + 1 )
+return A == 1
+for A in B :
+return A ** B
+A = B - C
+return A [ B % 10 ]
+break
+A = [ 0 , 1 ]
+A = 365
+if A and B % C == 0 :
+if len ( A ) == 0 :
+for A in range ( 2 , 20 ) :
+A -= 3
+A = max ( A , B [ C * 20 + D ] * B [ C * 20 + D + 1 ] * B [ C * 20 + D + 2 ] * B [ C * 20 + D + 3 ] )
+for A in range ( 21 ) :
+return max ( A , B )
+return sum ( range ( 1 , A + 1 ) )
+A = 0
+A , B = B , A % B
+A = [ 1 ] * 21
+if A < 10 :
+return A + 1
+if A == 2 :
+for A in range ( 2 , int ( 2000000 ** 0.5 ) + 1 ) :
+for A in range ( 5 ) :
+A += len ( B [ int ( C / 100 ) ] )
+if A :
+return ( A - 1 , B + 1 )
+A = ( int ( B / 2 ) , B + 1 )
+A [ B + 1 ] = A [ B ] * ( B + 1 )
+while A % B == 0 :
+if ( A % 400 == 0 or ( ( not A % 100 == 0 ) and A % 4 == 0 ) ) and B == 1 :
+return A ** int ( B ** ( 1 / A ) )
+A = A / B
+A *= B
+A = int ( A / 10 )
+for A in range ( B [ - 1 ] , C + 1 ) :
+return A < B
+A [ 1 ] = 0
+return A * A
+A = A * B
+A *= B [ C ]
+for A in range ( int ( 20 ** ( 1 / B ) ) ) :
+A = B . split ( )
+for A in range ( 1 , 1001 ) :
+A . insert ( 0 , B % 10 )
+while A <= B :
+while int ( A / 10 ) > 0 :
+return A - B
+while A != 0 :
+return A * A < B
+return int ( A / 2 ) if A % 2 == 0 else A * 3 + 1
+while A < 500 :
+for A in range ( B ) :
+A += 1
+for A in reversed ( B ) :
+for A in range ( 1 , 1000 ) :
+A += B
+return A == B
+for A in range ( B , 1000 ) :
+A = - 2 * int ( B / 100 ) + int ( int ( B / 100 ) / 4 )
+return B if A < B else A
+A = int ( A / 2 ) if A % 2 == 0 else A * 3 + 1
+A . append ( A [ - 1 ] + A [ - 2 ] )
+A . append ( int ( B ) )
+return int ( A [ B ] / ( A [ C ] * A [ B - C ] ) )
+A = B * B + C * C
+A . append ( B )
+if 4 <= A and B <= 16 :
+if A == B :
+A = [ 1 ] * 4 + [ 0 ] * 96
+if A < B :
+A = (str) ( B * C )
+A = ( B , int ( ( B + 1 ) / 2 ) )
+A = max ( A , B * C )
+A += len ( 'onethousand' )
+A = A + 1
+A += len ( B [ int ( C / 10 ) ] )
+if A > int ( B / 2 ) :
+A = len ( B )
+return ( A + 1 , B + 1 )
+if A == B * B and C + D + B == 1000 :
+A . append ( ( B , C [ B ] ) )
+A *= ( B + 1 )
+return A >= 0
+return int ( A )
+while A < B :
+return len ( A )
+A = sum ( range ( 1 , B + 1 ) )
+A = [ 2 ]
+for A in range ( B + B , C + 1 , B ) :
+if A < 2 :
+A . append ( B [ C + D ] )
+A += 12
+for A in range ( B - 1 ) :
+A /= B
+A = 600851475143
+A += int ( B )
+A += len ( 'hundredand' )
+A /= 10
+for A in range ( len ( B ) ) :
+return len ( A ) == 0
+if A <= 1 :
+A += B [ len ( B ) - 1 - C ]
+return A [ B ]
+A = 2
+for A in range ( len ( B [ C ] ) ) :
+return ( A + 1 , B )
+for A in range ( B , C ) :
+if A [ B ] :
+while A > 0 :
+A = [ 0 ] * ( B - len ( C ) + 1 )
+return (int) ( A / 2 ) + ( A % 2 )
+return True
+A = ''
+A = [ B ]
+A = B - 2
+return A
+for A in range ( B ) :
+return ( A + 1 , B - 1 )
+A = [ 0 ] * 21
+return ( A - 1 , B )
+A = [ B [ 0 ] , B [ - 1 ] ]
+A = [ - 1 ] * B
+return not A
+return A == 1000
+return list ( range ( A ) )
+A = [ 1 ] * ( B + 1 )
+A = 10 ** 6
+A = [ B for B in range ( 2 , int ( math . sqrt ( C ) ) + 1 ) if C % B == 0 ]
+A [ B ] = int ( C [ 20 ] / ( C [ B ] * C [ 20 - B ] ) )
+for A in range ( 2 , int ( B ** 0.5 ) + 1 ) :
+return A * B * C
+for A in range ( 5 , len ( B ) ) :
+A . pop ( 0 )
+if A == ' ' :
+A += B [ len ( B ) - C - 1 ]
+return A == 0
+A = max ( A , B [ C * 20 + D ] * B [ ( C + 1 ) * 20 + D - 1 ] * B [ ( C + 2 ) * 20 + D - 2 ] * B [ ( C + 3 ) * 20 + D - 3 ] )
+A [ B ] = C
+return A <= 0
+A = B
+return int ( A / B )
+if 0 < A <= 2 :
+A = str ( sum ( B ) )
+A = ( B + int ( 26 * ( C + 1 ) / 10 ) + ( D % 100 ) + int ( ( D % 100 ) / 4 ) + E ) % 7
+A . append ( A [ - 1 ] + B )
+if A % 7 == 6 :
+if A != 0 and A <= 20 :
+return sum ( range ( A , B ) )
+return A [ - 1 ]
+if A % 2 == 0 :
+for A in range ( 20 ) :
+for A in range ( 1901 , 2001 ) :
+if A % B == 0 :
+A = euler015_pascal_triangle ( B - 1 )
+for A in reversed ( range ( len ( B ) - 1 ) ) :
+A = [ True ] * B
+A [ B ] = False
+return A * A + B * B == C * C
+A . append ( int ( B [ C ] ) )
+return A % B == 0
+return A % 2 == 0
+for A in range ( 2 , 2000000 ) :
+for A , B in enumerate ( C ) :
+A = False
+return False
+return ( A - 1 , B - 1 )
+A = 1
+for A in range ( 100 ) :
+A += len ( B [ C ] )
+while A * A < B :
+A [ B ] [ C ] += max ( A [ B + 1 ] [ C ] , A [ B + 1 ] [ C + 1 ] )
+return A * B
