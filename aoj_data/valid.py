5523d6235ee84e76005bdc3357fe7a4b888c1751
Yuka Akinobu <sx2_vn_yuka@outlook.jp>
2021-01-06 13:24:27 +0900

    added aoj_data

---
+++ aoj_data/valid.py
@@ -0,0 +1,221 @@
+A = { }
+for A in range ( 9 , - 1 , - 1 ) :
+while A + 1 < B :
+A = 323
+A = B * C + D * E - F * G + H * I
+A , B = - 1 , 0
+C [ A ] . append ( int ( B [ 2 ] ) )
+A = 'L'
+A . edge [ B ] . append ( [ C , D , E ] )
+return abs ( A ) / 2
+# BをAから30を引いた値に160を掛けた値だけ増加させる
+import re
+A = 1 - B
+A . append ( complex ( B + C * D , E - C * F ) )
+for A in range ( B + 1 , 2 * C + 1 ) :
+A , B = len ( C ) , 0
+if A [ B ] [ C ] > D :
+for A in [ 'o' , 'x' ] :
+A , B = A + 1 , B + 1
+A , B = C [ 10 ] , C [ 11 ]
+if A [ B ] [ 2 ] :
+A = [ [ 0 for B in range ( C ) ] for D in range ( 2 ) ]
+A , B = { } , [ ]
+if A == 'U' :
+A = [ [ 0 for B in range ( 21 ) ] for C in range ( 2 ) ]
+print ( * A )
+A = 1e-11
+return A [ 0 : B - 1 ]
+A = B >> 1
+A = B [ 0 ] * B [ 5 ] - B [ 3 ] * B [ 2 ]
+A [ B ] [ C ] [ D ] += A [ B - 1 ] [ E ] [ F ] / 4
+# 入力された文字列の両端から空白改行を取り除いた文字列のリストをAとする
+A = [ [ 0 for B in range ( 15 ) ] for C in range ( 15 ) ]
+A , B = C [ D ] - 1 , C [ D + 1 ] - 1
+A = [ 0 ] + [ int ( input ( ) ) for B in range ( C ) ]
+for A in range ( B - C + 1 ) :
+A = complex ( 0 , 1 )
+A = [ int ( input ( ) ) for B in range ( 5 ) ]
+A [ B ] += C
+A += B [ 0 ]
+if A [ B ] [ 0 ] <= C and C <= A [ B ] [ 1 ] :
+print ( A + max ( B , C , D ) )
+A = B [ 1 ] = 2
+# 繰り返しを中断する
+# AをBとする
+# DのB番目のC番目をDのBから1を引いた値番目のC番目だけ増加させる
+while A and A * B [ 0 ] [ 0 ] < C :
+A , B , C = ord ( D ) - ord ( 'A' ) , ord ( E ) - ord ( 'A' ) , ord ( F ) - ord ( 'A' )
+A = convex_hull ( B )
+A , B = calc ( )
+# 繰り返しを中断する
+print ( * [ A // B for A in C ] )
+A , B = [ ] , len ( C )
+A += 7 * B
+# 真を返す
+return 'A'
+A , B , C = int ( A ) , int ( B ) , int ( C )
+A = ( A + B ) % C
+# 1をBとする
+A = B + 3
+A = set ( input ( ) . split ( ) )
+print ( ( A + 1 ) >> 1 )
+A = ( B - C ) * ( B - C ) + ( D - E ) * ( D - E )
+A = ( B - C [ 1 ] ) ** 2 + ( D - C [ 2 ] ) ** 2 + ( E - C [ 3 ] ) ** 2
+A = B % C
+return - 1 if A > 0 else B
+A , B = A - 1 , 0
+A = [ [ [ 0 for B in range ( 8 ) ] for C in range ( 8 ) ] for D in range ( 4 ) ]
+for A in range ( 5 * B ) :
+A [ B + 1 ] [ C - 1 ] = A [ B ] [ C - 2 ] + D [ C - 1 ]
+A = [ 41412121 , 14121214 , 41212141 , 12121414 , 21214141 , 12141412 , 21414121 , 14141212 ]
+A = str ( B ** C ) [ : : - 1 ]
+A = 2 - A
+# Aの先頭を展開してを出力する
+A [ 1 ] = [ 0 ] * B
+A += [ B + ( C << 9 ) for C in range ( D ) ]
+if A [ B ] == '=' :
+if A < len ( B ) :
+A = B [ 0 ]
+A , B , C , D , E , F = map ( int , input ( ) . split ( ) )
+A = B = C
+A [ B ] . append ( ( C , D ) )
+A = B [ A ] [ int ( C ) ]
+for A in range ( 6 ) :
+# 0をBの[MASK]番目のA番目にする
+A = 0x7fffff
+A = [ [ 0 for B in range ( 2 ) ] for C in range ( D ) ]
+A = int ( B ** 0.5 )
+A = ord ( 'a' )
+A , B = list ( input ( ) ) , 0
+A = [ [ B for C in range ( D ) ] for E in range ( D ) ]
+print ( A - 1 )
+A , B = C [ D ] - C [ E ] , F [ D ] - F [ E ]
+A , B = dijkstra ( C , D , C - 1 )
+print ( A % 3 + 1 )
+if A [ B ] . isdigit ( ) and A [ B + 1 ] . isdigit ( ) :
+A , B , C , D = list ( map ( int , input ( ) . split ( ',' ) ) )
+A [ B ] = A [ B - 1 ] + math . atan ( 1 / C [ B - 1 ] )
+while A [ B ] < C :
+A = complex ( B [ 4 ] , B [ 5 ] )
+A = nlcm ( B , C )
+print ( A [ B >> 1 ] )
+A . append ( complex ( B , C ) )
+A = [ B ] * ( C + 2 )
+A = list ( input ( ) . split ( ) )
+return
+A = min ( B , C )
+if A [ B ] == '@' :
+def d2t ( d ) return ( A // 100 ) * 60 + ( A % 100 )
+for A in range ( B , 11 ) :
+A = B [ A ]
+# Cを1だけ増加させる
+# 2をAとする
+A , B = 0 , C + D
+A . iter [ B ] = C
+A = B . bisect_left ( C , D )
+for A in range ( 12 ) :
+A = B + 4
+A = dot ( B - C [ 0 ] , D ) / norm ( D )
+A = '(J*)(O*)(I*)'
+A = [ [ ] for B in range ( 45 ) ]
+# 繰り返しを中断する
+A = 1
+return A . real * B . real + A . imag * B . imag
+A = [ 0 ] * 120
+A [ B ] [ 1 ] = 1
+A [ B ] [ C ] = D + 1
+print ( A * math . sin ( B / A ) * math . cos ( C * B / A ) / math . cos ( ( C - 1 ) * B / A ) )
+A , B , C = 0 , int ( D ) , C + 1
+# AのB番目、辞書のAのB番目番目を出力する
+A . append ( ( ( 0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 ) , 0 ) )
+A = [ B [ C : C + 3 ] for C in range ( len ( B ) - 2 ) ]
+# CのA番目をCのAからBの2乗を引いた値番目だけ増加させる
+combi ( 2 )
+A = list ( map ( int , input ( ) . split ( ',' ) ) )
+for A in range ( 1 , 4 ) :
+for A in range ( 16 ) :
+A = int ( input ( ) ) + 1
+A = str ( int ( B [ C : C + 4 ] [ : : - 1 ] ) )
+A . append ( ( B , C , D ) )
+A = [ 0 for B in range ( C + 2 ) ]
+A [ B + 1 ] [ C ] |= 1
+if A == '*' :
+print ( * [ ( '{}:{:02}' . format ( * A ) ) for A in sorted ( B ) ] )
+# Cを1だけ増加させる
+A = [ 0 ] * 4
+# BをAの0を取り出した値だけ増加させる
+print ( A [ 0 ] , A [ 1 ] , sep = '\n' )
+A [ B ] , C [ B ] = 0 , 1
+# A、'0'をB、Cとする
+A [ 0 ] [ 0 ] = 1
+# Fを1だけ増加させる
+print ( 'Case ' , A , ':' , sep = '' )
+if isPrime ( A ) :
+A -= 2 * B
+print ( A [ B ] [ C ] , D [ B ] [ C ] , sep = '\n' )
+A = min ( B [ C ] )
+A [ B ] [ 1 ] -= 1
+A = [ [ B for C in range ( 101 ) ] for D in range ( 101 ) ]
+A = [ [ 0 for B in range ( C ) ] for D in range ( 8 ) ]
+A = ( ( B [ C ] + D [ C ] ) << 2 ) + ( E [ C ] << 3 ) + E [ C ]
+A [ 1 - ( B & 1 ) ] = ''
+A [ B + 1 ] [ C ] = 1 - E if D == 2 else E
+for A in range ( 13 ) :
+print ( 'OK' if A else 'NG' )
+A = [ 0 ] * 1001
+while A < len ( B ) and C :
+A [ B - 1 ] [ C + 1 ] += 1
+A = ( B * C ) // ( C + D )
+while A [ 0 ] > 0 and A [ 1 ] > 0 :
+A = B . split ( '&' )
+print ( A - sum ( B [ C : A + 1 ] ) )
+A , B , C = int ( D [ 1 ] + D [ 2 ] , 16 ) , int ( D [ 3 ] + D [ 4 ] , 16 ) , int ( D [ 5 ] + D [ 6 ] , 16 )
+A , B = int ( C [ D ] ) , 0
+# 繰り返しを中断する
+# BをAだけ増加させる
+print ( A [ 0 ] [ B + 1 ] - 1 )
+# 繰り返しを中断する
+A = ( B + C ) >> 1
+A = UnionSet ( B + 1 )
+A = { '+' : B , '-' : C , '*' : D , '/' : E }
+A . append ( list ( map ( int , input ( ) . split ( ) ) ) )
+A = min ( B [ C ] , D - E )
+A = [ 0 , 0 , 0 , 1 , 1 , 1 , 2 , 2 , 2 ]
+# Aを24だけ増加させる
+A , B = { } , { }
+A = dot ( B [ 1 ] - B [ 0 ] , C [ 1 ] - C [ 0 ] )
+if len ( A ) and A [ 0 ] == B :
+print ( sum ( A [ 0 : 4 ] ) - min ( A [ 0 : 4 ] ) + max ( A [ 4 ] , A [ 5 ] ) )
+A = [ [ 0 for B in range ( 27 ) ] for C in range ( 27 ) ]
+print ( A - 1 - B )
+return complex ( ( A [ 0 ] . real * B - A [ 1 ] . real * C ) / ( B - C ) , ( A [ 0 ] . imag * B - A [ 1 ] . imag * C ) / ( B - C ) )
+# 繰り返しを中断する
+continue
+A = A [ : B ]
+for A in range ( 1 , 6 ) :
+for A , B , C in D :
+A [ 1 ] [ B ] = 1
+print ( A if A <= 0 else A - ( B - C ) )
+A , B , C = [ 0 ] , [ 0 ] , [ 0 ]
+A = bisect_left ( B , C [ 1 ] )
+A = B // C [ D ]
+print ( A % B )
+# 関数処理を中断する
+if re . match ( A , B ) :
+A , B , C = D . edge [ E ] [ F ]
+A , B = C . root ( D ) , C . root ( E )
+# DのA番目をCとする
+# Dを1だけ増加させる
+A -= 2
+A = [ 71.0 , 77.0 , 83.0 , 89.0 , 105.0 , 116.0 , 148.0 , 1000.0 ]
+A = max ( B [ C ] - D , E [ C ] - F )
+print ( 'OK' )
+A , B = list ( map ( int , input ( ) . split ( ) ) )
+# BのC番目をAとする
+A = ( 500 , 100 , 50 , 10 , 5 , 1 )
+for A in range ( B - C , B ) :
+A , B , C = D + E [ F ] [ 0 ] , G + E [ F ] [ 1 ] , H + E [ F ] [ 2 ]
+A [ B ] [ C ] [ D ] [ E ] += A [ B ] [ C - 1 ] [ D ] [ D ]
+if A > 2 :
+A = sorted ( B . items ( ) , key = lambda x : A [ 1 ] )
