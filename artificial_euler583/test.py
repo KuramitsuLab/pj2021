d47a23e11b1dc66c3467c974a4f698741b667fb3
Emi <sakura.math.sashimo@gmail.com>
2021-01-21 11:04:28 +0900

    add non-duplicated artificial_euler

---
+++ artificial_euler583/test.py
@@ -0,0 +1,59 @@
+facts [ x + 1 ] = facts [ x ] * ( x + 1 ) 
+h = ( d + int ( 26 * ( m + 1 ) / 10 ) + ( y % 100 ) + int ( ( y % 100 ) / 4 ) + gamma ) % 7
+return primes
+n = int ( n / 10 ) 
+n = n / i
+i = i + 1
+def euler006_make_common_diff_sequence ( s , d , n ) : 
+buf = \" \" 
+while b != 0 : 
+primes = [ True ] * ( n + 1 ) 
+def euler012_is_list_empty_a ( l ) : 
+for i in range ( s , e + 1 ) : 
+def euler008 ( ) : 
+for j in range ( i , 1000 ) : 
+sep = ( int ) ( len ( s ) / 2 ) + ( len ( s ) % 2 ) 
+q = str ( 2**1000 ) 
+ret = i
+fl += l
+sl . append ( buf ) 
+ret . append ( l [ s + i ] ) 
+a = 1
+return a * a < n
+for i in range ( 2 , int ( a**0 . 5 ) + 1 ) : 
+def euler011_over_or_equal ( a , b ) : 
+ret += s [ len ( s ) - 1 - i ] 
+a , b = b , a % b
+p = i * i + j * j
+n /= a
+def euler006_n_square_a ( n ) : 
+if m < n : 
+n = int ( n / 2 ) if n % 2 == 0 else n * 3 + 1
+for i in reversed ( range ( len ( tri ) - 1 ) ) : 
+prime = [ ] 
+for i in pl : 
+if m < N : 
+fl . append ( ( x , c ) ) 
+def euler013_front_n_slice ( s , n ) : 
+else : 
+def euler012_prime_factorize ( n ) : 
+if 0 < n <= 2 : 
+def euler016 ( ) : 
+def euler014 ( ) : 
+elif a == 0 : 
+q . append ( int ( ch ) ) 
+ret = \" \" 
+ret = [ ] 
+return word [ n % 10 ] 
+cl [ 1 ] = 0
+elif n == 2 : 
+return a + 1
+if n == 1 : 
+while n % i == 0 : 
+primes = [ True ] * ( n + 1 ) 
+dl = [ ] 
+while a % x == 0 : 
+return ret
+if 4 <= j and i <= 16 : 
+return a
+fact = 1
